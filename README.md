59cbca14bd6951ba12a467b9288a805ea858f840
Vincenzo0709 <vmerola99@gmail.com>
2025-04-02 12:58:31 +0200

    Refactor directories

--- README.md
+++ README.md
@@ -1,31 +1,9 @@
 # Vitis HLS dot product
-This repository automates IP building for Vitis HLS dot product component.
-You can clone all git repository or copy only fetch_sources.sh.
+This repository automates IP building for Vitis HLS accelerators.
 It is all arranged to be UninaSoC directories-compliant.
 
-### Hardware build
-To compile HLS and package the IP:
+You can clone all git repository or use only scripts/fetch_sources.sh.
 
-    make syn
-    make package
-
-from hw/ directory.
-
-Or you can call
-
-    ./scripts/fetch_sources.sh
-
-To extract only rtl sources (in Verilog), ip directory for Vivado and ip zip.
-Outputs will be located in build/ directory.
-
-### Application build
-The bare-metal application is already arranged to be UninaSoC directories-compliant.
-You can test if it compiles by using Makefile (you need riscv bare-metal toolchain).
-
-After importing on UninaSoC some files must be deleted:
-* startup/ dir 
-* Makefile_test is not needed for UninaSoC
-
-To test compilation:
-
-    make -f Makefile_test
+### Accelerators
+- vdotprod: vector dot product
+- matmul: GEMM
--- scripts/fetch_sources.sh
+++ scripts/fetch_sources.sh
@@ -2,7 +2,7 @@
 # Author: Vincenzo Merola <vincenzo.merola2@unina.it>
 
 # Description:
-# This script downloads Vitis HLS vdotprod sources and flattens them into an rtl/ directory.
+# This script downloads Vitis HLS accelerators sources and flattens them into a build/ directory.
 
 # To execute: ./fetch_sources.sh
 # To execute you need:
@@ -15,62 +15,77 @@ GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
+GIT_URL=https://github.com/Vincenzo0709/Vitis_HLS_accelerators.git
+GIT_TAG=main
+CLONE_DIR=Vitis_HLS_accelerators
+
+declare -A accel_tags
+accel_tags=(["vdotprod"]="v1.0" ["matmul"]="v1.0")
+
 # Display help
 Help()
 {
    echo
-   echo "This script downloads Vitis HLS vdotprod sources and flattens them into an rtl/ directory."
+   echo "This script downloads Vitis HLS accelerators sources and flattens them into build/ directory."
    echo
    echo "Syntax: fetch_source.sh [--option]"
    echo
    echo "options:"
+   echo "-a | --all             Builds all accelerators"
    echo "-h | --help            Prints help"
    echo
 }
 
-Flatten()
+GitFlatten()
 {
-    # Create build dir
-    if ! [ -d hw/ ]; then
-        mkdir hw
-    fi
-    cd hw
-    rm -rf build/
-    mkdir build/
-    mkdir build/rtl
-    mkdir build/ip
+    
+    BUILD="`pwd -P`/build"
 
-    # Clone repo and update submodule to specific branch
-    GIT_URL=https://github.com/Vincenzo0709/vdotprod.git
-    GIT_TAG=main
-    CLONE_DIR=vdotprod
-    HW=${CLONE_DIR}/hw
-
+    # Cloning repo and updating submodules to specific tag
     printf "${YELLOW}[FETCH_SOURCES] Cloning source repository${NC}\n"
     git clone ${GIT_URL} -b ${GIT_TAG} ${CLONE_DIR}
+    cd ${CLONE_DIR}
+    git submodule update --recursive
+
+    for i in ${ACC}; do
+
+        cd accel/$i/
+        git fetch
+        git checkout ${accel_tags[$i]}
+        cd ${WORK_DIR}
+        
+    done
+
 
     # Clone Bender (future development)
     # printf "${YELLOW}[FETCH_SOURCES] Download Bender${NC}\n"
     # curl --proto '=https' --tlsv1.2 https://pulp-platform.github.io/bender/init -sSf | sh
 
-    # Copy all RTL files into rtl dir
+    # Copying all build files
     printf "${YELLOW}[FETCH_SOURCES] Copying all sources into rtl${NC}\n"
-    cd ${HW}
-    make syn
-    cp -r vdotprod/hls/syn/verilog/* ../../build/rtl
-    make package
-    cp vdotprod_hls.zip ../../build/
-    cp -r vdotprod/hls/impl/ip/* ../../build/ip
-    cd ../..
+    for i in ${ACC}; do
 
-    # Delete the cloned repo
+        mkdir -p ${BUILD}/${i}/ip ${BUILD}/${i}/rtl
+
+        # Accessing each chosen accelerator and building
+        cd accel/${i}
+        ./scripts/fetch_sources.sh
+
+        # Copying results in build/ directory
+        cp -r build/* ${BUILD}/$i
+
+    done
+
+
+    # Deleting the cloned repo
     printf "${YELLOW}[FETCH_SOURCES] Cleaning all artifacts${NC}\n"
     sudo rm -r ${CLONE_DIR}
     printf "${GREEN}[FETCH_SOURCES] Completed${NC}\n"
+
 }
 
 
-OPTS=$(getopt -o h --long help -n 'fetch_sources.sh' -- "$@")
+OPTS=$(getopt -o ha --long,all help -n 'fetch_sources.sh' -- "$@")
 eval set -- "$OPTS"
 
 if [ "$OPTS" != " --" ]; then
@@ -80,6 +95,12 @@ if [ "$OPTS" != " --" ]; then
                 # Print help
                 Help
                 exit 0
+                ;;
+            -a | --all)
+                # All accelerators
+                ACC="vdotprod matmul"
+                shift
+                break
                 ;;
             --)
                 shift
@@ -97,9 +118,12 @@ fi
 
 # Moving to right directory (if you cloned the whole repository instead of using only fetch_sources.sh)
 cd "$(dirname "$0")" ; printf "\n${GREEN}[FETCH_SOURCES] Starting from directory `pwd -P`${NC}\n"
+WORK_DIR=`pwd -P`
 if [ "$(basename `pwd -P`)" == "scripts" ]; then
-    cd .. ; printf "${GREEN}[FETCH_SOURCES] Moving to directory `pwd -P`${NC}\n"
+    cd .. ; printf "\n${GREEN}[FETCH_SOURCES] Moving to directory `pwd -P`${NC}\n"
+    WORK_DIR=`pwd -P`
+    Flatten
+else
+    GitFlatten
 fi
-
-Flatten
 
